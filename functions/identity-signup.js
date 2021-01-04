978ceb780e16c3e20f23e3cd6ff2e082acc873b3
Suraj Bahadur <surajbahadur58@gmail.com>
2021-01-04 08:15:01 +0530

    fauna db connectivity added

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,7 +1,38 @@
+const fetch = require('node-fetch');
+
+
+
 exports.handler = async (event) => {
+
 
     const {user} = JSON.parse(event.body);
     console.log(JSON.stringify(user, null, 2));
+
+    const netlifyID = user.id;
+
+    const stripeID = 1;
+
+    const response = await fetch("https://graphql.fauna.com/graphql", {
+        method : 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+        },
+        body : JSON.stringify({
+            query: `
+            mutation($netlifyID: ID!, stripeID: ID!) {
+                createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID})
+            }
+            `,
+            variables : {
+                netlifyID,
+                stripeID,
+            }
+        })
+    }).then(res => res.json())
+    .catch( (err) = console.log(err));
+
+    console.log({response})
+    ;
 
     return {
         // every serverless function has to return status code and body
